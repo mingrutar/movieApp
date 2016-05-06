2a1cac8b8c60fd5ba59c82d29b16e92081c44644
ming <rutar.ming@gmail.com>
2016-05-05 21:20:49 -0700

    still not working

--- app/src/main/java/com/coderming/movieapp/GridViewAdapter.java
+++ app/src/main/java/com/coderming/movieapp/GridViewAdapter.java
@@ -2,16 +2,16 @@ package com.coderming.movieapp;
 
 import android.app.Activity;
 import android.content.Context;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
-import android.widget.TextView;
 
 import com.squareup.picasso.Picasso;
-import android.support.v7.widget.RecyclerView;
+
 import java.util.Collection;
 import java.util.List;
 
@@ -64,11 +64,10 @@ public class GridViewAdapter extends ArrayAdapter<MovieItem> {
     }
     class CustomerViewHolder extends RecyclerView.ViewHolder {
         ImageView mImageView;
-        TextView mTextView;
 
         public CustomerViewHolder(View itemView) {
             super(itemView);
-            mImageView = (ImageView) itemView.findViewById(R.id.movie_grid);
+            mImageView = (ImageView) itemView.findViewById(R.id.movie_poster);
         }
     }
 
